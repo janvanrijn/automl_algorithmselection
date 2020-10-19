acb37a4cbd67a88a9a60ea85913e70e645fffa62
janvanrijn <janvanrijn@gmail.com>
2020-10-19 15:34:30 +0200

    added assignment files

---
+++ assignment.py
@@ -0,0 +1,136 @@
+import numpy as np
+import pandas as pd
+
+import sklearn.tree
+import sklearn.ensemble
+
+import typing
+
+
+class AbsractMetaLearningModel(object):
+
+    def fit(self, df_features: pd.DataFrame, df_performance: pd.DataFrame) -> None:
+        """
+        Takes an input (meta-features) and trains the internal model on it.
+
+        :param df_features: pd.DataFrame
+        a data frame with size (N, M), where all N rows represent a base-dataset, and all M columns represent a
+        meta-feature, calculated over that specific dataset
+
+        :param df_performance: pd.DataFrame
+        a data frame with size (N, D), where all N rows represent a base-dataset, and all D columns represent the
+        performance of a (base-)model on that specific dataset (predictive accuracy)
+        """
+        raise NotImplementedError("Abstract Method, please subclass")
+
+    def predict(self, df_features: pd.DataFrame) -> typing.List[str]:
+        """
+        Predicts for a set of datasets (expressed in meta-features) the performance per (base-)model
+
+        :param df_features: pd.DataFrame
+        a data frame with size (N, M), where all N rows represent a base-dataset, and all M columns represent a
+        meta-feature, calculated over that specific dataset
+
+        :return: List[str]
+        A list of length N, where each item in the list represents the name of the (base-)model that is predicted to
+        perform best
+        """
+        raise NotImplementedError("Abstract Method, please subclass")
+
+    def score(self, y: pd.DataFrame, y_hat: typing.List[str]) -> typing.List[str]:
+        """
+        Scores the how well the by the meta-model selected classifiers would have performed.
+
+        :param y: pd.DataFrame
+        a data frame with size (N, D), where all N rows represent a base-dataset, and all D columns represent the
+        performance of a (base-)model on that specific dataset (predictive accuracy)
+
+        :param y_hat: pd.DataFrame
+        A list of length N, where each item in the list represents the name of the (base-)model that is predicted to
+        perform best
+
+        :return: List[float]
+        The performance of the by the meta-model selected classifiers (indicated in y_hat) per task
+        """
+        result = []
+        for idx, classifier in enumerate(y_hat):
+            result.append(y[classifier][idx])
+        return result
+
+
+class MetaLearningBestOnAverage(AbsractMetaLearningModel):
+
+    def __init__(self, _):
+        """
+        Baseline method, that determines during fit time which method performs best on average. This method is selected
+        always at test time.
+        """
+        self.best_model_name = None
+
+    def fit(self, df_features: pd.DataFrame, df_performance: pd.DataFrame) -> None:
+        """
+        Takes an input (meta-features) and trains the internal model on it.
+
+        :param df_features: pd.DataFrame
+        a data frame with size (N, M), where all N rows represent a base-dataset, and all M columns represent a
+        meta-feature, calculated over that specific dataset
+
+        :param df_performance: pd.DataFrame
+        a data frame with size (N, D), where all N rows represent a base-dataset, and all D columns represent the
+        performance of a (base-)model on that specific dataset (predictive accuracy)
+        """
+        raise NotImplementedError('Please implement')
+
+    def predict(self, df_features: pd.DataFrame) -> typing.List[str]:
+        """
+        Predicts for a set of datasets (expressed in meta-features) the performance per (base-)model
+
+        :param df_features: pd.DataFrame
+        a data frame with size (N, M), where all N rows represent a base-dataset, and all M columns represent a
+        meta-feature, calculated over that specific dataset
+
+        :return: List[str]
+        A list of length N, where each item in the list represents the name of the (base-)model that is predicted to
+        perform best
+        """
+        raise NotImplementedError('Please implement')
+
+
+class MetaLearningRegressionBased(AbsractMetaLearningModel):
+
+    def __init__(self, expected_models):
+        """
+        Baseline method, that determines during fit time which method performs best on average. This method is selected
+        always at test time.
+        """
+        self.models = {
+            model: sklearn.ensemble.RandomForestRegressor(random_state=0) for model in expected_models
+        }
+
+    def fit(self, df_features: pd.DataFrame, df_performance: pd.DataFrame) -> None:
+        """
+        Takes an input (meta-features) and trains the internal model on it.
+
+        :param df_features: pd.DataFrame
+        a data frame with size (N, M), where all N rows represent a base-dataset, and all M columns represent a
+        meta-feature, calculated over that specific dataset
+
+        :param df_performance: pd.DataFrame
+        a data frame with size (N, D), where all N rows represent a base-dataset, and all D columns represent the
+        performance of a (base-)model on that specific dataset (predictive accuracy)
+        """
+        raise NotImplementedError('Please implement')
+
+    def predict(self, df_features: pd.DataFrame) -> typing.List[str]:
+        """
+        Predicts for a set of datasets (expressed in meta-features) the performance per (base-)model
+
+        :param df_features: pd.DataFrame
+        a data frame with size (N, M), where all N rows represent a base-dataset, and all M columns represent a
+        meta-feature, calculated over that specific dataset
+
+        :return: List[str]
+        A list of length N, where each item in the list represents the name of the (base-)model that is predicted to
+        perform best
+        """
+        raise NotImplementedError('Please implement')
---
+++ test.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import unittest
+
+from solution import MetaLearningBestOnAverage, MetaLearningRegressionBased
+
+
+class TestMetaModels(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.meta_features = [
+            'MajorityClassSize',
+            'MaxNominalAttDistinctValues',
+            'MinorityClassSize',
+            'NumberOfClasses',
+            'NumberOfFeatures',
+            'NumberOfInstances',
+            'NumberOfInstancesWithMissingValues',
+            'NumberOfMissingValues',
+            'NumberOfNumericFeatures',
+            'NumberOfSymbolicFeatures'
+        ]
+        self.classifier_features = [
+            'AdaBoostClassifier(DecisionTreeClassifier)',
+            'BernoulliNB',
+            'DecisionTreeClassifier',
+            'ExtraTreesClassifier',
+            'GradientBoostingClassifier',
+            'KNeighborsClassifier',
+            'MLPClassifier',
+            'RandomForestClassifier',
+            'SGDClassifier',
+            'SVC'
+        ]
+
+    def assertArrayAlmostEqual(self, results, fixture):
+        for r, f in zip(results, fixture):
+            self.assertAlmostEqual(r, f)
+
+    def test_models(self):
+        df_meta = pd.read_csv('metadata.csv')
+        df_classifiers = df_meta[self.classifier_features]
+        df_metaf = df_meta[self.meta_features]
+
+        test_size = 10
+        train_features = df_metaf[test_size:]
+        train_results = df_classifiers[test_size:]
+        test_features = df_metaf[:test_size].reset_index()
+        del test_features['index']
+        test_results = df_classifiers[:test_size].reset_index()
+        del test_results['index']
+
+        baseline = MetaLearningBestOnAverage(self.classifier_features)
+        baseline.fit(train_features, train_results)
+        predictions = baseline.predict(test_features)
+        score_baseline = baseline.score(test_results, predictions)
+        fixture_baseline = [0.9953066332916144, 0.9538, 0.9616, 0.976, 0.828, 0.9713876967095852, 0.9645, 0.747,
+                            0.8045, 0.5369993211133739]
+        self.assertArrayAlmostEqual(score_baseline, fixture_baseline)
+
+        regression_based = MetaLearningRegressionBased(self.classifier_features)
+        regression_based.fit(train_features, train_results)
+        predictions = regression_based.predict(test_features)
+        score_regression = regression_based.score(test_results, predictions)
+        fixture_regresion = [0.9953066332916144, 0.9487, 0.7504000000000001, 0.9765, 0.828, 0.9713876967095852, 0.9645, 0.747, 0.8045, 0.5668703326544466]
+
+        self.assertArrayAlmostEqual(score_regression, fixture_regresion)
